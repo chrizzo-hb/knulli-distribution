26af874bd6ed98188256bd2fda278eeae07dd088
Chrizzo <chrizzo@unterderbruecke.de>
2025-01-15 03:27:07 +0100

    Removed redundant LED achievement scripts

--- board/batocera/fsoverlay/usr/share/emulationstation/scripts/achievements/led.sh
+++ board/batocera/fsoverlay/usr/share/emulationstation/scripts/achievements/led.sh
@@ -2,7 +2,7 @@
 
 BOARD=$(cat /boot/boot/batocera.board)
 # We only want the script to run for these devices
-if [ "$BOARD" != "rg40xx-h" ] && [ "$BOARD" != "rg40xx-v" ] && [ "$BOARD" != "rg-cubexx" ]; then
+if [ "$BOARD" != "rg40xx-h" ] && [ "$BOARD" != "rg40xx-v" ] && [ "$BOARD" != "rg-cubexx" ] && [ "$BOARD" != "trimui-smart-pro" ] && [ "$BOARD" != "trimui-brick" ]; then
     exit 1
 fi
 
